7474f15cc08edc14323c201e7051ab974e7a8c9f
Richard Woodburn <richardkw@me.com>
2022-12-26 17:02:03 +1100

    Initial Commit

---
+++ melbournedrains/melbournedrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  melbournedrainsApp.swift
+//  melbournedrains
+//
+//  Created by Richard Woodburn on 26/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct melbournedrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
